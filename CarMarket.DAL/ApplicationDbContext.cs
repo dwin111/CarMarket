565e91908bef56de5a48d1f891de102cc07827e3
mrdey <mr.deymys.12@gmail.com>
2022-11-26 01:36:25 +0200

    Add project files.

---
+++ CarMarket.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarMarket.Domain.Models_Entity_;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarMarket.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Car> Car { get; set; }
+    }
+}
---
+++ CarMarket.Domain/Enum/TypeCar.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CarMarket.Domain.Enum
+{
+    public enum TypeCar
+    {
+        [Display(Name = "Passenger car")]
+        PassengerCar = 0,
+
+        [Display(Name = "Sedan")]
+        Sedan = 1,
+
+        [Display(Name = "HatchBack")]
+        HatchBack = 2,
+
+        [Display(Name = "Minivan")]
+        Minivan = 3,
+
+        [Display(Name = "Sport car")]
+        SportCar = 4,
+
+        [Display(Name = "Suv")]
+        Suv = 5,
+    }
+}
