682d9d4705c92f864ffd80a244f5bace40e6da8c
Дима Дудченко <mr.deymys.12@gmail.com>
2022-11-27 01:30:23 +0200

    HashHelper

---
+++ CarMarket.Domain/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMarket.Domain.Helpers
+{
+    public static class HashPasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using(var sha256 = SHA256.Create())
+            {
+                var hashedByte = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hash = BitConverter.ToString(hashedByte).Replace("-","").ToLower();
+                return hash;
+            }
+        }
+    }
+}
